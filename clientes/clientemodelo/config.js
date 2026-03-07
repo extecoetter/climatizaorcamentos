4e14c457280ceaf51054c86180a125d37585484b
extecoetter <extecoetterrobson@gmail.com>
2026-03-06 21:36:42 -0300

    Update config.js

--- clientes/clientemodelo/config.js
+++ clientes/clientemodelo/config.js
@@ -1,8 +1,8 @@
 window.CLIENT_CONFIG = {
   installer: {
-    nome: "Robson Andre Extecoetter",
+    nome: "Cliente Modelo Modelo",
     whats: "(14) 99707-3447",
-    pix: "14 99707 3447 / Robson A Extecoetter"
+    pix: "14 99707 3447 / Cliente Modelo"
   },
 
   logo: "/climatizaorcamentos/clientes/clientemodelo/logo.png"
