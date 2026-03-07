ca61cc80ecbc11cf7fad3f02600f53f03d5b0cc5
extecoetter <extecoetterrobson@gmail.com>
2026-03-06 21:49:51 -0300

    Add files via upload

---
+++ clientes/androidteste/config.js
@@ -0,0 +1,9 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Cliente Modelo Modelo",
+    whats: "(14) 99707-3447",
+    pix: "14 99707 3447 / Cliente Modelo"
+  },
+
+  logo: "/climatizaorcamentos/clientes/clientemodelo/logo.png"
+};
